5cba60e99a183f5e35c2483a6a3eae6d5234e91d
Rishabh <rishabhbhardwaj1234567@gmail.com>
2020-02-03 19:32:30 +0530

    beats imp

--- app/src/main/java/com/sih/policeapp/Activities/Beats.java
+++ app/src/main/java/com/sih/policeapp/Activities/Beats.java
@@ -124,26 +124,28 @@ public class Beats extends AppCompatActivity implements OnMapReadyCallback,Googl
                         }
                     };
 
-                    if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        // TODO: Consider calling
-                        //    Activity#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for Activity#requestPermissions for more details.
-                        return;
-                    } else {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                            lastKnown = getLastKnownLocation();
-                        }
-                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-                        if (lastKnown != null) {
-                           // lastKnown = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                            myref.setValue(lastKnown.getLatitude() + "||" + lastKnown.getLongitude());
-                        }
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    Activity#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for Activity#requestPermissions for more details.
+                            return;
+                        } else {
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                lastKnown = getLastKnownLocation();
+                            }
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                            if (lastKnown != null) {
+                               // lastKnown = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                                myref.setValue(lastKnown.getLatitude() + "||" + lastKnown.getLongitude());
+                            }
 
 
+                        }
                     }
                 }
             }
