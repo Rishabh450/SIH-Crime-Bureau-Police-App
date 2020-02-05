c1dd2d4f405fce25d8c600fe0e6ba78b9aa780b4
Rishabh <rishabhbhardwaj1234567@gmail.com>
2020-02-05 17:32:26 +0530

    image search completed

---
+++ app/src/main/java/com/sih/Utils/CompareImage.java
@@ -0,0 +1,225 @@
+package com.sih.Utils;
+
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+
+import javax.net.ssl.SSLException;
+
+
+public class CompareImage extends AsyncTask<Void, Void, Void>
+{
+    Context context;
+    File file,file2;
+
+    public CompareImage(Context context, File file, File file2) {
+        this.context = context;
+        this.file = file;
+        this.file2 = file2;
+        pdLoading = new ProgressDialog(context);
+    }
+
+    double confidence=0.0;
+    private final static int CONNECT_TIME_OUT = 30000;
+    private final static int READ_OUT_TIME = 50000;
+    final File sdCard = Environment.getExternalStorageDirectory();
+
+    private  String getBoundary() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for(int i = 0; i < 32; ++i) {
+            sb.append("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-".charAt(random.nextInt("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_".length())));
+        }
+        return sb.toString();
+    }
+    private  String encode(String value) throws Exception{
+        return URLEncoder.encode(value, "UTF-8");
+    }
+
+    public  byte[] getBytesFromFile(File f) {
+        if (f == null) {
+            return null;
+        }
+        try {
+            FileInputStream stream = new FileInputStream(f);
+            ByteArrayOutputStream out = new ByteArrayOutputStream(1000);
+            byte[] b = new byte[1000];
+            int n;
+            while ((n = stream.read(b)) != -1)
+                out.write(b, 0, n);
+            stream.close();
+            out.close();
+            return out.toByteArray();
+        } catch (IOException e) {
+        }
+        return null;
+    }
+
+    protected  byte[] post(String url, HashMap<String, String> map, HashMap<String, byte[]> fileMap) throws Exception {
+        String boundaryString = getBoundary();
+        HttpURLConnection conne;
+        URL url1 = new URL(url);
+        conne = (HttpURLConnection) url1.openConnection();
+        conne.setDoOutput(true);
+        conne.setUseCaches(false);
+        conne.setRequestMethod("POST");
+        conne.setConnectTimeout(CONNECT_TIME_OUT);
+        conne.setReadTimeout(READ_OUT_TIME);
+        conne.setRequestProperty("accept", "*/*");
+        conne.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundaryString);
+        conne.setRequestProperty("connection", "Keep-Alive");
+        conne.setRequestProperty("user-agent", "Mozilla/4.0 (compatible;MSIE 6.0;Windows NT 5.1;SV1)");
+        DataOutputStream obos = new DataOutputStream(conne.getOutputStream());
+        Iterator iter = map.entrySet().iterator();
+        while(iter.hasNext()){
+            Map.Entry<String, String> entry = (Map.Entry) iter.next();
+            String key = entry.getKey();
+            String value = entry.getValue();
+            obos.writeBytes("--" + boundaryString + "\r\n");
+            obos.writeBytes("Content-Disposition: form-data; name=\"" + key
+                    + "\"\r\n");
+            obos.writeBytes("\r\n");
+            obos.writeBytes(value + "\r\n");
+        }
+        if(fileMap != null && fileMap.size() > 0){
+            Iterator fileIter = fileMap.entrySet().iterator();
+            while(fileIter.hasNext()){
+                Map.Entry<String, byte[]> fileEntry = (Map.Entry<String, byte[]>) fileIter.next();
+                obos.writeBytes("--" + boundaryString + "\r\n");
+                obos.writeBytes("Content-Disposition: form-data; name=\"" + fileEntry.getKey()
+                        + "\"; filename=\"" + encode(" ") + "\"\r\n");
+                obos.writeBytes("\r\n");
+                obos.write(fileEntry.getValue());
+                obos.writeBytes("\r\n");
+            }
+        }
+        obos.writeBytes("--" + boundaryString + "--" + "\r\n");
+        obos.writeBytes("\r\n");
+        obos.flush();
+        obos.close();
+        InputStream ins = null;
+        int code = conne.getResponseCode();
+        try{
+            if(code == 200){
+                ins = conne.getInputStream();
+            }else{
+                ins = conne.getErrorStream();
+            }
+        }catch (SSLException e){
+            e.printStackTrace();
+            return new byte[0];
+        }
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buff = new byte[4096];
+        int len;
+        while((len = ins.read(buff)) != -1){
+            baos.write(buff, 0, len);
+        }
+        byte[] bytes = baos.toByteArray();
+        ins.close();
+        return bytes;
+    }
+    ProgressDialog pdLoading;
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Log.d("confidencekitna"," loading" );
+        //this method will be running on UI thread
+        pdLoading.setMessage("\tComparing...");
+        pdLoading.show();
+    }
+    @Override
+    protected Void doInBackground(Void... params) {
+
+        //this method will be running on background thread so don't update UI frome here
+        //do your long running http tasks here,you dont want to pass argument and u can access the parent class' variable url over here
+        URL url = null;
+        try {
+            url = new URL("https://firebasestorage.googleapis.com/v0/b/policeapp-397c4.appspot.com/o/-M-HJEv6_No92bBpXlU5%2Fprofile_pic.jpg?alt=media&token=7e9898f4-e141-4c8a-9fce-baa01585be6c");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            // File file = Paths.get(url.toURI()).toFile();
+            //File file = null;
+            //FileUtils.copyURLToFile(url, file);
+
+            //file = new File(sdCard.getAbsolutePath() + "/HashContact"+"/Pictures/"+"13.jpg");
+            byte[] buff1 = getBytesFromFile(file);
+            Log.d("confidencekitna","1");
+            //file2 = new File(sdCard.getAbsolutePath() + "/HashContact"+"/Pictures/"+"11.jpg");
+
+            // Create a new file object for the second file and get bytes from file
+            //File file2 = new File("C:\\Users\\ihene\\Desktop\\esan-caleb.jpg");
+            byte[] buff2 = getBytesFromFile(file2);
+            Log.d("confidencekitna","2");
+            // Data needed to use the Face++ Compare API
+            String url1 = "https://api-us.faceplusplus.com/facepp/v3/compare";
+            HashMap<String, String> map = new HashMap<>();
+            HashMap<String, byte[]> byteMap = new HashMap<>();
+            map.put("api_key", "zADIkrvwyv23nZ2UX8_wVFZc2cHRfM-L");
+            map.put("api_secret", "G1GVBBJLSsyoo_c5ZvxcZ1U7HIXlRigH");
+
+            byteMap.put("image_file1", buff1);
+            byteMap.put("image_file2", buff2);
+
+            try {
+                // Connecting and retrieving the JSON results
+                byte[] bacd = post(url1, map, byteMap);
+                Log.d("confidencekitna","4");
+                String jsonStr = new String(bacd);
+                Log.d("confidencekitna","5");
+                // Parse the JSON and get the confidence value
+                JSONObject obj = new JSONObject(jsonStr);
+                Log.d("confidencekitna","6");
+                confidence = obj.getDouble("confidence");
+                System.out.println("kitnaconf" +confidence);
+                //Toast.makeText(MainActivity.this,"Match:"+confidence,Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Log.d("confidencekitna", String.valueOf(e));
+                e.printStackTrace();
+            }
+        }
+
+
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+        Log.d("confidencekitna"," "+confidence);
+        if(confidence>0.0)
+            Toast.makeText(context,"Match:"+confidence,Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(context,"Timeout,Check your Internet connection also ensure file size <2 mb",Toast.LENGTH_LONG).show();
+        //            //this method will be running on UI thread
+
+        pdLoading.dismiss();
+    }
+
+}
--- app/src/main/java/com/sih/policeapp/MainActivity.java
+++ app/src/main/java/com/sih/policeapp/MainActivity.java
@@ -8,12 +8,16 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,9 +32,17 @@ import com.google.firebase.auth.UserInfo;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.onesignal.OneSignal;
+import com.sih.Utils.CompareImage;
 import com.sih.policeapp.Activities.Beats;
 import com.sih.policeapp.Activities.Login;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
@@ -148,6 +160,12 @@ authStateListener=new FirebaseAuth.AuthStateListener() {
                     Intent intent = new Intent(MainActivity.this,AddCriminalActivity.class);
                     startActivity(intent);
                 }
+                if(menuItem.getItemId() == R.id.search){
+                    CropImage.activity()
+                            .setGuidelines(CropImageView.Guidelines.ON)
+                            .setAspectRatio(1,1)
+                            .start(MainActivity.this);
+                }
                 if(menuItem.getItemId() == R.id.logout){
 
                     mAuth.signOut();
@@ -171,7 +189,69 @@ authStateListener=new FirebaseAuth.AuthStateListener() {
     int index=0;
 
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
 
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                Uri imageUri = data.getData();
+                FileOutputStream outStream = null;
+                try {
+                   Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+                    long t = System.currentTimeMillis();
+                    String time = String.valueOf(t);
+                        final File sdCard = Environment.getExternalStorageDirectory();
+                        //File file = new File(sdCard.getAbsolutePath() + "/HashContact"+"/ComparedPhoto/"+time+".jpg");
+
+                            //Bitmap bit = BitmapFactory.decodeResource(getResources(), R.drawable.chatback);
+                            Log.d("pathssss", "onPictureTaken - wrote to ");
+
+
+                            File dir = new File(sdCard.getAbsolutePath() + "/HashContact" + "/ComparedPhoto");
+                            dir.mkdirs();
+
+                            String fileName = time+".jpg";
+                            fileName.trim();
+                          //  bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+                            Log.d("pathsssss", "onPictureTaken - wrote to " + fileName);
+
+                            File outFile = new File(dir, fileName);
+                    outStream = new FileOutputStream(outFile);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 20, outStream);
+                    outStream.flush();
+                    outStream.close();
+                            Log.d("pathsssss", "onPictureTaken - wrote to " + fileName + dir);
+                    File file = new File(sdCard.getAbsolutePath() + "/HashContact"+"/ComparedPhoto/"+fileName);
+                   // File sdCardRoot = Environment.getExternalStorageDirectory();
+                    File yourDir = new File(sdCard.getAbsolutePath() + "/HashContact" + "/Pictures");
+                    for (File f : yourDir.listFiles()) {
+                        if (f.isFile())
+                        {
+
+                            CompareImage example=new CompareImage(this,file,f);
+                            example.execute();
+                        }
+                        // Do your stuff
+                    }
+
+
+                } catch (IOException e) {
+                    Toast.makeText(this, "Something Went Wrong...", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+
+
+
+
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
     public void setUpToolBar()
     {
         mDrawerLayout = findViewById(R.id.drawer);
